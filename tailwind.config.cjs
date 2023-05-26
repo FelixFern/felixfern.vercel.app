192c2992256d6b0b90e30e1b5430f5fc684fbf60
Felix Fernando <felixzhuang1@gmail.com>
2023-05-26 13:06:51 +0700

    feat: start of hero section

---
+++ tailwind.config.cjs
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+    theme: {
+        extend: {
+            colors: {
+                main: "#141414",
+                accent: "#FF985F",
+                textmain: "#FFFFFF",
+                textsecondary: "#E1E1E1",
+            },
+            letterSpacing: {
+                widest: ".25rem",
+            },
+        },
+    },
+    plugins: [],
+};
