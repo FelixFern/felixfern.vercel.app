bfe6101c388359fb6218920ab481556779545a49
Felix Fernando <felixzhuang1@gmail.com>
2023-05-29 11:28:24 +0700

    feat: added small animation on greeting part and start for work experience section

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,10 +1,18 @@
-import React from "react";
+import React, { useState } from "react";
 import BlurCircle from "./BlurCircle/BlurCircle.jsx";
 import Button from "./Button/Button.jsx";
-import { motion, useScroll, useTransform } from "framer-motion";
+import {
+    AnimatePresence,
+    motion,
+    useScroll,
+    useTransform,
+} from "framer-motion";
 import { useEffect, useRef } from "react";
 
+const greetList = ["HELLO.", "HI.", "HALO.", "你好"];
+
 const Hero = () => {
+    const [greet, setGreet] = useState("HELLO.");
     const targetRef = useRef(null);
     const { scrollYProgress } = useScroll({
         target: targetRef,
@@ -14,6 +22,12 @@ const Hero = () => {
     const opacity = useTransform(scrollYProgress, [0, 0.5], [1, 0]);
     const scale = useTransform(scrollYProgress, [0, 0.5], [1, 0.5]);
     const translateY = useTransform(scrollYProgress, [0, 0.5], [0, -100]);
+
+    useEffect(() => {
+        setInterval(() => {
+            setGreet(greetList[Math.floor(Math.random() * greetList.length)]);
+        }, 1500);
+    }, []);
 
     return (
         <motion.div
@@ -26,13 +40,37 @@ const Hero = () => {
                 animate={{ opacity: 1 }}
                 transition={{ duration: 0.5, ease: "easeIn" }}
                 style={{ scale, translateY }}
+                className="relative"
             >
-                <p className="font-black text-xs flex gap-2 items-center">
-                    <span className="px-1.5 py-0.5 bg-white text-main">
-                        HELLO.
-                    </span>{" "}
+                <p
+                    className={`font-black text-xs flex gap-1 items-center transition-all duration-300 ${
+                        greetList.indexOf(greet) === 0
+                            ? "ml-[3.75rem] transition-all"
+                            : greetList.indexOf(greet) === 1
+                            ? "ml-[2.05rem] transition-all"
+                            : greetList.indexOf(greet) === 2
+                            ? "ml-[3.35rem] transition-all"
+                            : "ml-10 transition-all"
+                    }`}
+                >
+                    <AnimatePresence>
+                        <motion.span
+                            key={greet}
+                            className="px-1.5 py-0.5 bg-white text-main absolute left-0"
+                            initial={{ y: [0, 10], opacity: 0, scale: 0.9 }}
+                            animate={{ y: [10, 0], opacity: 1, scale: 1 }}
+                            exit={{ y: [0, -15], opacity: 0, scale: 0.9 }}
+                            transition={{
+                                duration: 0.5,
+                                ease: "easeIn",
+                            }}
+                        >
+                            {greet}
+                        </motion.span>{" "}
+                    </AnimatePresence>
                     my name is
                 </p>
+
                 <div className="text-center">
                     <h1 className="font-black text-5xl">Felix Fernando</h1>
                     <h2 className="font-medium tracking-widest">
--- src/components/WorkExp.jsx
+++ src/components/WorkExp.jsx
@@ -1,7 +1,20 @@
 import React from "react";
+import BlurCircle from "./BlurCircle/BlurCircle";
 
 const WorkExp = () => {
-    return <div>WorkExp</div>;
+    return (
+        <div className="w-screen h-screen relative">
+            <div className="w-5/6 h-screen mx-auto">
+                <h1 className="font-black text-4xl">
+                    Work <span className="text-accent">Experience.</span>
+                </h1>
+            </div>
+
+            <BlurCircle classes={"h-[60vh] w-[60vh] top-[50%] left-[-10%]"} />
+            <BlurCircle classes={"h-[30vh] w-[30vh] top-[0%] right-[-15%]"} />
+            <div></div>
+        </div>
+    );
 };
 
 export default WorkExp;
