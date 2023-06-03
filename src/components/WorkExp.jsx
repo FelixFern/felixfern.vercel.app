229a106525928fcf0381076f1c8385ff26bb59bd
felixfern <felixzhuang1@gmail.com>
2023-06-03 20:48:32 +0700

    feat: animation adjustment for work experience list

--- src/components/WorkExp.jsx
+++ src/components/WorkExp.jsx
@@ -1,59 +1,72 @@
-import React from "react";
+import React, { useRef } from "react";
 import BlurCircle from "./BlurCircle/BlurCircle";
 import WorkExperienceList from "./WorkExperienceList/WorkExperienceList";
+import { useScroll, useTransform, motion } from "framer-motion";
 
 const work_exp = [
-    {
-        company: "TurnkeyID",
-        position: "Frontend Engineer Intern",
-        location: "Hybrid - Bandung, West Java",
-        period: "Dec 2022 - Mar 2023 (3 Months)",
-        desc: [
-            "Consumed and integrated API to the front using RESTful API and GraphQL.",
-            "Worked closely with a team of frontend developers, backend developers, and UX designer on developing products for TurnkeyID group.",
-            "Handled the frontend on one of TurnkeyID group web application product using Next.js and built PWA out of the web application.",
-            "Maintained, fixed, and improved the TurnkeyID group web application product on the frontend side.",
-        ],
-    },
-    {
-        company: "Diceritain",
-        position: "Software Engineer Intern",
-        location: "Remote",
-        period: "Aug 2022 - Dec 2022 (4 Months)",
-        desc: [
-            "Refactored Diceritain android mobile app frontend code for better user interface and user experience.",
-            "Optimized Firebase data fetching on specific features in the Diceritain Android mobile app.",
-            "Implemented automatic email-sending feature for completed payment using Firebase extension.",
-            "Worked closely with the product team to improve and maintain Diceritain mobile android app.",
-        ],
-    },
-    {
-        company: "Matematika ITB",
-        position: "Wordpress Developer",
-        location: "Hybrid - Bandung, West Java",
-        period: "Jul 2022 - Mar 2023 (8 Months)",
-        desc: [
-            "Maintained Mathematics ITB website content using WordPress and Elementor.",
-            "Developed and Designed websites for Mathematics ITB Combinatorics International Virtual Courses and 8th International Conference on Mathematics and Natural Sciences using Elementor, both website is used as the main sources of information for the events.",
-        ],
-    },
+	{
+		company: "TurnkeyID",
+		position: "Frontend Engineer Intern",
+		location: "Hybrid - Bandung, West Java",
+		period: "Dec 2022 - Mar 2023 (3 Months)",
+		desc: [
+			"Consumed and integrated API to the front using RESTful API and GraphQL.",
+			"Worked closely with a team of frontend developers, backend developers, and UX designer on developing products for TurnkeyID group.",
+			"Handled the frontend on one of TurnkeyID group web application product using Next.js and built PWA out of the web application.",
+			"Maintained, fixed, and improved the TurnkeyID group web application product on the frontend side.",
+		],
+	},
+	{
+		company: "Diceritain",
+		position: "Software Engineer Intern",
+		location: "Remote",
+		period: "Aug 2022 - Dec 2022 (4 Months)",
+		desc: [
+			"Refactored Diceritain android mobile app frontend code for better user interface and user experience.",
+			"Optimized Firebase data fetching on specific features in the Diceritain Android mobile app.",
+			"Implemented automatic email-sending feature for completed payment using Firebase extension.",
+			"Worked closely with the product team to improve and maintain Diceritain mobile android app.",
+		],
+	},
+	{
+		company: "Matematika ITB",
+		position: "Wordpress Developer",
+		location: "Hybrid - Bandung, West Java",
+		period: "Jul 2022 - Mar 2023 (8 Months)",
+		desc: [
+			"Maintained Mathematics ITB website content using WordPress and Elementor.",
+			"Developed and Designed websites for Mathematics ITB Combinatorics International Virtual Courses and 8th International Conference on Mathematics and Natural Sciences using Elementor, both website is used as the main sources of information for the events.",
+		],
+	},
 ];
 
 const WorkExp = () => {
-    return (
-        <div className="w-screen h-screen relative">
-            <div className="w-5/6 h-screen mx-auto">
-                <div className="w-8/12 h-screen mx-auto">
-                    <h1 className="font-black text-4xl">
-                        Work <span className="text-accent">Experience.</span>
-                    </h1>
-                    <WorkExperienceList data={work_exp}></WorkExperienceList>
-                </div>
-            </div>
-            <BlurCircle classes={"h-[60vh] w-[60vh] top-[50%] left-[-10%]"} />
-            <BlurCircle classes={"h-[30vh] w-[30vh] top-[0%] right-[-15%]"} />
-        </div>
-    );
+	const targetRef = useRef(null);
+	const { scrollYProgress } = useScroll({
+		target: targetRef,
+		offset: ["start end", "end start"],
+	});
+
+	const translateX = useTransform(scrollYProgress, [0, 0.5], [-1000, 0]);
+
+	return (
+		<div ref={targetRef} className="w-screen h-fit relative">
+			<motion.div style={{ translateX }} className="w-5/6 h-fit mx-auto ">
+				<div className="w-8/12 h-screen mx-auto after:contents after:w-10 after:h-2 after:bg-white flex flex-col justify-center ">
+					<h1 className="font-black text-4xl">
+						Work <span className="text-accent">Experience.</span>
+					</h1>
+					<motion.div>
+						<WorkExperienceList
+							data={work_exp}
+						></WorkExperienceList>
+					</motion.div>
+				</div>
+			</motion.div>
+			<BlurCircle classes={"h-[20vh] w-[20vh] bottom-[0%] left-[0%]"} />
+			<BlurCircle classes={"h-[30vh] w-[30vh] top-[0%] right-[-10%]"} />
+		</div>
+	);
 };
 
 export default WorkExp;
--- src/components/WorkExperienceList/WorkExperienceList.jsx
+++ src/components/WorkExperienceList/WorkExperienceList.jsx
@@ -1,56 +1,64 @@
 import React, { useState } from "react";
-import { motion } from "framer-motion";
+import { AnimatePresence, motion } from "framer-motion";
 
 const WorkExperienceList = ({ data }) => {
-    const [currentIndex, setCurrentIndex] = useState(0);
-
-    return (
-        <div className="w-11/12 mx-auto flex gap-4 mt-6">
-            <div className="flex flex-col gap-3 h-fit border-l-[1px] border-white py-1 justify-center w-2/12">
-                {data.map((work, idx) => (
-                    <div className="relative px-4">
-                        <h2
-                            className={`${
-                                currentIndex === idx
-                                    ? "font-black text-accent"
-                                    : ""
-                            } cursor-pointer transition-colors duration-200`}
-                            onClick={() => {
-                                setCurrentIndex(idx);
-                            }}
-                        >
-                            {work.company}
-                        </h2>
-                        {idx === currentIndex ? (
-                            <motion.div
-                                className="absolute left-[-2.5px] top-[0.125rem] h-5 w-1 bg-accent"
-                                layoutId="indicator"
-                            />
-                        ) : null}
-                    </div>
-                ))}
-            </div>
-            <div className="w-10/12">
-                <h2 className="font-black">
-                    {data[currentIndex].position}{" "}
-                    <span className="text-accent font-bold">
-                        @{data[currentIndex].company}
-                    </span>
-                </h2>
-                <h3 className="text-sm">
-                    {data[currentIndex].location} |{" "}
-                    <span className="font-bold">
-                        {data[currentIndex].period}
-                    </span>
-                </h3>
-                <ul className="list-disc text-sm mt-3 ml-5">
-                    {data[currentIndex].desc.map((desc) => (
-                        <li>{desc}</li>
-                    ))}
-                </ul>
-            </div>
-        </div>
-    );
+	const [currentIndex, setCurrentIndex] = useState(0);
+	return (
+		<div className="w-11/12 mx-auto flex gap-4 mt-6">
+			<div className="flex flex-col gap-3 h-fit border-l-[1px] border-white py-1 justify-center w-2/12">
+				{data.map((work, idx) => (
+					<div className="relative px-4" key={work.company}>
+						<h2
+							className={`${
+								currentIndex === idx
+									? "font-black text-accent"
+									: ""
+							} cursor-pointer transition-colors duration-200`}
+							onClick={() => {
+								setCurrentIndex(idx);
+							}}
+						>
+							{work.company}
+						</h2>
+						{idx === currentIndex ? (
+							<motion.div
+								className="absolute left-[-2.5px] top-[0.125rem] h-5 w-1 bg-accent"
+								layoutId="indicator"
+							/>
+						) : null}
+					</div>
+				))}
+			</div>
+			<AnimatePresence mode="wait">
+				<motion.div
+					initial={{ opacity: 0 }}
+					animate={{ opacity: 1 }}
+					exit={{ opacity: 0 }}
+					transition={{ duration: 0.2, ease: "ease" }}
+					key={data[currentIndex].company}
+					className="w-10/12"
+				>
+					<h2 className="font-black">
+						{data[currentIndex].position}{" "}
+						<span className="text-accent font-bold">
+							@{data[currentIndex].company}
+						</span>
+					</h2>
+					<h3 className="text-sm">
+						<span className="font-bold">
+							{data[currentIndex].location} |{" "}
+							{data[currentIndex].period}
+						</span>
+					</h3>
+					<ul className="list-disc text-sm mt-3 ml-5 w-full">
+						{data[currentIndex].desc.map((desc) => (
+							<li key={desc}>{desc}</li>
+						))}
+					</ul>
+				</motion.div>
+			</AnimatePresence>
+		</div>
+	);
 };
 
 export default WorkExperienceList;
