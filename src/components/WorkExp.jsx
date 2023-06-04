d1776a76ac428e3f3cb9264e02b0ed5e829b7695
felixfern <felixzhuang1@gmail.com>
2023-06-04 12:02:13 +0700

    feat: added more animation for work experience list

--- src/components/WorkExp.jsx
+++ src/components/WorkExp.jsx
@@ -47,25 +47,40 @@ const WorkExp = () => {
 		offset: ["start end", "end start"],
 	});
 
-	const translateX = useTransform(scrollYProgress, [0, 0.5], [-1000, 0]);
+	const scale = useTransform(scrollYProgress, [0, 0.5], [0.9, 1]);
 
 	return (
-		<div ref={targetRef} className="w-screen h-fit relative">
-			<motion.div style={{ translateX }} className="w-5/6 h-fit mx-auto ">
-				<div className="w-8/12 h-screen mx-auto after:contents after:w-10 after:h-2 after:bg-white flex flex-col justify-center ">
-					<h1 className="font-black text-4xl">
-						Work <span className="text-accent">Experience.</span>
-					</h1>
-					<motion.div>
-						<WorkExperienceList
-							data={work_exp}
-						></WorkExperienceList>
-					</motion.div>
+		<motion.div
+			ref={targetRef}
+			style={{ scale }}
+			className="w-screen h-fit relative"
+		>
+			<div className="w-5/6 h-fit mx-auto">
+				<div className="w-8/12 h-screen mx-auto flex flex-col justify-center ">
+					<div className="flex gap-4 justify-center items-center">
+						<h1 className="font-black text-4xl">
+							Work{" "}
+							<span className="text-accent">Experience.</span>
+						</h1>
+						<motion.div className="flex-grow">
+							<motion.div
+								initial={{ width: 0 }}
+								whileInView={{ width: "100%" }}
+								transition={{
+									duration: 0.2,
+									ease: "easeIn",
+									delay: ".2",
+								}}
+								className="h-1 bg-white"
+							></motion.div>
+						</motion.div>
+					</div>
+					<WorkExperienceList data={work_exp}></WorkExperienceList>
 				</div>
-			</motion.div>
+			</div>
 			<BlurCircle classes={"h-[20vh] w-[20vh] bottom-[0%] left-[0%]"} />
 			<BlurCircle classes={"h-[30vh] w-[30vh] top-[0%] right-[-10%]"} />
-		</div>
+		</motion.div>
 	);
 };
 
--- src/components/Works.jsx
+++ src/components/Works.jsx
@@ -1,7 +1,13 @@
 import React from "react";
 
 const Works = () => {
-	return <div className="h-screen">Works</div>;
+	return (
+		<div className="h-screen w-screen">
+			<div className="w-5/6 mx-auto">
+				<h1 className="font-black text-5xl">Works</h1>
+			</div>
+		</div>
+	);
 };
 
 export default Works;
